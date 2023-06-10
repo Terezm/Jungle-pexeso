d46ab89e6d54d87f786e073a45c09c89842af3d5
Lýdie Čiháková <lydiecihakova@gmail.com>
2023-06-10 18:37:19 +0200

    player-score

--- src/components/Score/index.jsx
+++ src/components/Score/index.jsx
@@ -1,1 +1,17 @@
-const Score = () => {};
+import React from 'react';
+import './style.css';
+
+export const Score = ({ players }) => {
+  return (
+    <div className="score-container">
+      {players.map((player) => {
+        return (
+          <div key={player.name} className="score-item">
+            <div className="score-name">{player.name}</div>
+            <div className="score-value">{player.score}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- src/views/pexeso/index.jsx
+++ src/views/pexeso/index.jsx
@@ -3,6 +3,7 @@ import { Card } from './card';
 import { useSearchParams } from 'react-router-dom';
 import { generateGame } from './generateGame';
 import './style.css';
+import { Score } from '../../components/Score';
 
 export const Pexeso = () => {
   const [searchParams] = useSearchParams();
@@ -10,11 +11,15 @@ export const Pexeso = () => {
   const mode = searchParams.get('mode');
   const size = searchParams.get('size');
 
-  const players =
-    mode === 'single'
-      ? [{ name: 'Hráč 1' }]
-      : [{ name: 'Hráč 1' }, { name: 'Hráč 2' }];
   const [playerIndex, setPlayerIndex] = useState(0);
+  const [players, setPlayers] = useState(
+    mode === 'single'
+      ? [{ name: 'Hráč 1', score: 0 }]
+      : [
+          { name: 'Hráč 1', score: 0 },
+          { name: 'Hráč 2', score: 0 },
+        ],
+  );
 
   function nextPlayer() {
     const nextIndex = (playerIndex + 1) % players.length;
@@ -38,43 +43,50 @@ export const Pexeso = () => {
 
   let deduction;
   return (
-    <div>
-      <h1>{currentPlayer.name}</h1>
-      <div className={`pexeso-container game${size}`}>
-        {fieldCards.map((card, i) => (
-          <Card
-            key={i}
-            isSwitched={
-              first !== card && second !== card && !match.includes(card?.id)
-            }
-            onTurn={() => {
-              if (first === null && second === null) {
-                setFirst(card);
-              } else if (second === null) {
-                setSecond(card);
-                if (first.img === card.img) {
-                  match.push(first.id);
-                  match.push(card.id);
-                  setMatch(match);
-                  clearTimeout(deduction);
-                } else {
-                  deduction = setTimeout(() => {
-                    clearSelection();
-                    nextPlayer();
-                  }, 5000);
-                }
-              } else {
-                nextPlayer();
-                clearTimeout(deduction);
-                setFirst(card);
-                setSecond(null);
-              }
-            }}
-            img={card.img}
-          />
-        ))}
+    <div className="pexeso-split">
+      <div>
+        <Score players={players} />
       </div>
-      {first?.id} - {second?.id}
+      <div className="pexeso-container">
+        <h1>{currentPlayer.name}</h1>
+        <div className={`pexeso-game game${size}`}>
+          {fieldCards.map((card, i) => (
+            <Card
+              key={i}
+              isSwitched={
+                first !== card && second !== card && !match.includes(card?.id)
+              }
+              onTurn={() => {
+                if (first === null && second === null) {
+                  setFirst(card);
+                } else if (second === null) {
+                  setSecond(card);
+                  if (first.img === card.img) {
+                    match.push(first.id);
+                    match.push(card.id);
+                    setMatch(match);
+                    clearTimeout(deduction);
+                    currentPlayer.score += 1;
+                    setPlayers([...players]);
+                  } else {
+                    deduction = setTimeout(() => {
+                      clearSelection();
+                      nextPlayer();
+                    }, 5000);
+                  }
+                } else {
+                  nextPlayer();
+                  clearTimeout(deduction);
+                  setFirst(card);
+                  setSecond(null);
+                }
+              }}
+              img={card.img}
+            />
+          ))}
+        </div>
+        {first?.id} - {second?.id}
+      </div>
     </div>
   );
 };
