1e86a72136c5e2f9b34bc3a1e3bd13983d864d80
Tereza Maninová <maninova69@gmail.com>
2023-06-10 11:19:45 +0200

    animal list update

---
+++ src/components/Animals/index.jsx
@@ -0,0 +1,251 @@
+export const animals = [
+  {
+    img: "/img/dolphin.jpg",
+    name: "Delfín",
+    description:
+      "Delfín je inteligentní a společenský mořský savec. Má štíhlé tělo, ploutve a dlouhý zobák.",
+    habitat: "Delfíni žijí v oceánech po celém světě.",
+    diet: "Delfíni se živí rybami a korýši.",
+  },
+  {
+    img: "/img/giraffe.jpg",
+    name: "Žirafa",
+    description:
+      "Žirafa je vysoký a dlouhokrký savec s dlouhými nohama a skvrnitým vzorem na srsti.",
+    habitat: "Žirafy žijí v otevřených savanách a lesích v Africe.",
+    diet: "Žirafy se živí listy a větvemi stromů.",
+  },
+  {
+    img: "/img/elephant.jpg",
+    name: "Slon",
+    description:
+      "Slon je největší suchozemský savec s dlouhým chobotem a velkými kly.",
+    habitat:
+      "Sloni se vyskytují v různých typech prostředí, včetně lesů, savan a bažin.",
+    diet: "Sloni se živí trávou, větvemi, plody a kůrou stromů.",
+  },
+  {
+    img: "/img/gorilla.jpg",
+    name: "Gorila",
+    description:
+      "Gorila je velký a silný primát s robustním tělem a silnými končetinami.",
+    habitat:
+      "Gorily obývají lesy a horské oblasti ve Střední a Východní Africe.",
+    diet: "Gorily se živí převážně rostlinnou stravou, jako jsou listy, výhonky a ovoce.",
+  },
+  {
+    img: "/img/hroch.jpg",
+    name: "Hroch",
+    description:
+      "Hroch je těžký a robustní savec s velkou hlavou, tlustým tělem a velkými ústy.",
+    habitat:
+      "Hroši se vyskytují v řekách, jezerech a mokřadech subsaharské Afriky.",
+    diet: "Hroši se živí převážně rostlinnou stravou, jako je tráva, listy a plody.",
+  },
+  {
+    img: "/img/panda.jpg",
+    name: "Panda",
+    description:
+      "Panda je černobílý medvěd s charakteristickým vzhledem a potravou.",
+    habitat: "Pandy žijí v horách a lesích v Číně.",
+    diet: "Pandy se živí převážně bambusem.",
+  },
+  {
+    img: "/img/zebra.jpg",
+    name: "Zebra",
+    description:
+      "Zebra je africké zvíře s černým a bílým pruhovaným vzorem na srsti.",
+    habitat: "Zebry obývají otevřené savany a pastviny v Africe.",
+    diet: "Zebry se živí trávou a bylinami.",
+  },
+  {
+    img: "/img/tiger.jpg",
+    name: "Bílý tygr",
+    description:
+      "Bílý tygr je vzácná forma tygra s bílou srstí a tmavými pruhy.",
+    habitat: "Bílí tygři se vyskytují převážně v oblastech Indie.",
+    diet: "Bílí tygři se živí převážně masem.",
+  },
+  {
+    img: "/img/turtle.jpg",
+    name: "Vodní želva",
+    description:
+      "Vodní želva je želva přizpůsobená životu ve vodě s pancířem a ploutvemi.",
+    habitat: "Vodní želvy žijí v jezerech, řekách a mořích po celém světě.",
+    diet: "Vodní želvy se živí rostlinnou i živočišnou stravou, včetně vodních rostlin a malých živočichů.",
+  },
+  {
+    img: "/img/lion.jpg",
+    name: " Lev",
+    description:
+      "Lev je velká kočkovitá šelma ze savany. Má hnědou srst s černými skvrnami a dlouhé drápy.",
+    habitat: "Lev žije převážně v Africe, ve stepích a savanách.",
+    diet: "Lev se živí převážně masem, loví různá stáda zvířat.",
+  },
+
+  {
+    img: "/img/cheetah.jpg",
+    name: "Gepard",
+    description:
+      "Gepard je rychlý a elegantní šelma, která je známá svou schopností dosáhnout vysokých rychlostí během krátkých sprintů.",
+    habitat: "Afrika, části Asie",
+    diet: "Maso (loví menší savce)",
+  },
+  {
+    img: "/img/koala.jpg",
+    name: "Koala",
+    description:
+      "Koala je malý savec s šedou srstí, velkýma ušima a výraznými okrouhlýma očima. Je známý svou předností k eukalyptovým listům.",
+    habitat: "Austrálie",
+    diet: "Eukalyptové listy",
+  },
+  {
+    img: "/img/wolf.jpg",
+    name: "Vlk",
+    description:
+      "Vlk je velký dravec, který žije ve smečkách a je známý svou inteligencí a společenským chováním.",
+    habitat: "Severní Amerika, Evropa, Asie",
+    diet: "Maso (loví převážně jeleny, losy)",
+  },
+  {
+    img: "/img/kangaroo.jpg",
+    name: "Klokan",
+    description:
+      "Klokan je skákavý savec se silnými zadními končetinami a dlouhým ocasem. Je známý svou schopností přeskočit velké vzdálenosti pomocí skoků.",
+    habitat: "Austrálie a okolní ostrovy",
+    diet: "Tráva, listy, výhonky",
+  },
+  {
+    img: "/img/chameleon.jpg",
+    name: "Chameleon",
+    description:
+      "Chameleon je plaz s schopností měnit barvu své kůže, což mu pomáhá se maskovat a komunikovat.",
+    habitat: "Afrika, Madagaskar, jižní Evropa, Asie",
+    diet: "Hmyz, pavouci, malí obratlovci",
+  },
+
+  {
+    img: "/img/bear.jpg",
+    name: "Medvěd hnědý",
+    description:
+      "Medvěd hnědý je velký savec s robustním tělem a silnými tlapami. Je známý svou schopností hibernovat.",
+    habitat: "Severní Amerika, Evropa, Asie",
+    diet: "Tráva, kořínky, ovoce, ryby",
+  },
+
+  {
+    img: "/img/cat.jpg",
+    name: "Kočka domácí",
+    description:
+      "Kočka domácí je malý druh šelmy, který se často chová jako domácí mazlíček. Je známá svou nezávislostí a elegantním pohybem.",
+    habitat: "Domácnosti, farmy, městská prostředí",
+    diet: "Maso, suché krmivo",
+  },
+
+  {
+    img: "/img/bizon.jpg",
+    name: "Bizon",
+    description:
+      "Bizon je velký savec z čeledi turovitých. Má robustní tělo, silné končetiny a výrazný hrb na zádech. Je známý svou masivností a imponujícími rohy u obou pohlaví.",
+    habitat: "Severní Amerika (hlavně prérie a lesnaté oblasti)",
+    diet: "Tráva, byliny, větvičky, listy",
+  },
+
+  {
+    img: "/img/crocodile.jpg",
+    name: "Krokodýl nilský",
+    description:
+      "Krokodýl nilský je velký plaz se silnou tlamou a hřbetními hřebeny. Je známý svou schopností lovit ve vodě.",
+    habitat: "Afrika (převážně Nil)",
+    diet: "Ryby, ptáci, savci",
+  },
+  {
+    img: "/img/puma.jpg",
+    name: "Puma",
+    description:
+      "Puma je velká kočkovitá šelma se silnými svaly a špičatými ušima. Je známá svou obratností a loveckými schopnostmi.",
+    habitat: "Lesy a horské oblasti Ameriky",
+    diet: "Maso (loví převážně jeleny, kapybary)",
+  },
+  {
+    img: "/img/capybara.jpg",
+    name: "Kapybara",
+    description:
+      "Kapybara je největší žijící hlodavec na světě. Má robustní tělo, krátké nohy a charakteristicky ploché uši. Je známá svou společenskou povahou a schopností žít ve velkých skupinách.",
+    habitat: "Bažiny, pobřežní oblasti a říční nivy v Jižní Americe",
+    diet: "Tráva, vodní rostliny, listy, kůra",
+  },
+
+  {
+    img: "/img/surikata.jpg",
+    name: "Surikata",
+    description:
+      "Surikata je malá šelma známá svým společenským chováním a schopností hlídat okolí. Je často zobrazována ve filmech a animovaných seriálech.",
+    habitat: "Pouště a polopouště Jižní Afriky",
+    diet: "Hmyz, pavouci, menší obratlovci",
+  },
+  {
+    img: "/img/tucan.jpg",
+    name: "Tukan",
+    description:
+      "Tukan je pestře zbarvený pták s velkým zobákem. Je známý svým hlasitým křikem a je často spojován s pralesy Jižní Ameriky.",
+    habitat: "Pralesy Střední a Jižní Ameriky",
+    diet: "Ovoce, semena, hmyz",
+  },
+  {
+    img: "/img/penguin.jpg",
+    name: "Tučňák císařský",
+    description:
+      "Tučňák císařský je velký tučňák s charakteristickým oranžovým límcem kolem krku. Je známý svou schopností vydržet extrémní mrazy.",
+    habitat: "Antarktida",
+    diet: "Ryby, kril",
+  },
+  {
+    img: "/img/cobra.jpg",
+    name: "Kobra indická",
+    description:
+      "Kobra indická je jedovatý had s charakteristickou roztažitelnou královskou kapucí. Je známá svým výstražným chováním a jedovatým kousnutím.",
+    habitat: "Indický subkontinent a jeho okolí",
+    diet: "Malí savci, ptáci, plazi",
+  },
+  {
+    img: "/img/flamingo.jpg",
+    name: "Plameňák růžový",
+    description:
+      "Plameňák růžový je velký pták s dlouhými nohama a charakteristickou růžovou barvou peří. Je známý svou elegantní postavou a skupinovým chováním.",
+    habitat: "Slaná jezera a močály",
+    diet: "Raky, měkkýši, řasy",
+  },
+  {
+    img: "/img/tapir.jpg",
+    name: "Tapír",
+    description:
+      "Tapír je velký býložravý savec s charakteristickým pohyblivým chobotem. Je známý svou robustní stavbou těla a schopností plavat.",
+    habitat: "Deštné pralesy v Jižní a Střední Americe, jihovýchodní Asie",
+    diet: "Listy, výhonky, ovoce, tráva",
+  },
+  {
+    img: "/img/jaguar.jpg",
+    name: "Jaguár",
+    description:
+      "Jaguár je velká kočkovitá šelma s krátkými a silnými nohami. Je známý svou sílou a loveckými schopnostmi.",
+    habitat: "Deštné pralesy a bažiny v Americe",
+    diet: "Maso (loví převážně kapybary, opice)",
+  },
+  {
+    img: "/img/squirrel.jpg",
+    name: "Veverka",
+    description:
+      "Veverka je malý hlodavec s hustou srstí a dlouhým ocasem. Je známá svými skoky mezi stromy a schopností skladovat potravu.",
+    habitat: "Lesy a parky po celém světě",
+    diet: "Ořechy, semena, plody, pupeny",
+  },
+  {
+    img: "/img/black-panther.jpg",
+    name: "Černý panter",
+    description:
+      "Černý panter je melanistická forma leoparda nebo jaguára, která má černé zbarvení. Je známý svou elegancí, sílou a loveckými schopnostmi.",
+    habitat: "Různé typy lesů a horské oblasti",
+    diet: "Maso (loví převážně menší a středně velké savce)",
+  },
+];
