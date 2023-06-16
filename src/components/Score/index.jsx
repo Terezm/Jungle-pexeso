253e5ba855ccd069a388293c0366cff187362e44
Lýdie Čiháková <lydiecihakova@gmail.com>
2023-06-16 21:12:29 +0200

    pexeso and score center

--- src/components/Score/index.jsx
+++ src/components/Score/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './style.css';
-
 export const Score = ({ players }) => {
   return (
     <div className="score-container">
       {players.map((player) => {
         return (
-
-         
           <h1 key={player.name} className="score-item">
             <div className="score-name">{player.name}</div>
             <div className="score-value"> {player.score}</div>
